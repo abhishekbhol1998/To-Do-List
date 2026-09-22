a5dcaff7ed72c31b5e459bfa489ee994614a6d2a
abhishekbhol1998 <abhishekkbhol@gmail.com>
2026-09-22 12:45:16 +0530

    added dist using build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "To-Do-List",
+  base: "/To-Do-List/",
   plugins: [react()],
 });
